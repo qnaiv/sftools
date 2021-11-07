2c88ef86b571d57d3eac119a11fd0f2c0b29b74d
qnaiv <43468715+qnaiv@users.noreply.github.com>
2021-11-07 14:29:47 +0000

    サインアップ画面を自動入力する機能が動かない Fixes #45

--- src/contentScripts/index.js
+++ src/contentScripts/index.js
@@ -1,14 +1,50 @@
-import querystring from 'querystring'
+import { querySelectorDeep } from 'query-selector-shadow-dom';
 
-// eslint-disable-next-line no-restricted-globals
-const parsed = querystring.parse(location.search.slice(1))
-console.log(parsed);
-document.getElementById('last_name').value = parsed.lastName || ''
-document.getElementById('first_name').value = parsed.firstName || ''
-document.getElementById('email').value = parsed.email || ''
-document.getElementById('job_role').value = parsed.role || ''
-document.getElementById('job_role').value = parsed.role || ''
-document.getElementById('company').value = parsed.company || ''
-document.getElementById('country').value = parsed.country || ''
-document.getElementById('postal_code').value = parsed.postalCode || ''
-document.getElementById('username').value = parsed.userName || ''
+function waitForElement(selector, timeoutMs){
+    const startTimeInMs = Date.now();
+    return new Promise((resolve)=>{
+        function findLoop(){
+            const elements = querySelectorDeep(selector);
+            if(elements != null){
+                resolve(elements);
+            }
+            setTimeout(()=>{
+                if(Date.now() - startTimeInMs > timeoutMs) resolve(null);
+                findLoop();
+            }, 1000)
+        }
+        findLoop();
+    })
+}
+async function main(){
+    try{
+        const params = new URLSearchParams(window.location.search);
+
+        const lastNameElm = await waitForElement("input[name='last_name']", 1000);
+        if(lastNameElm) lastNameElm.value = params.get("lastName") || '';
+
+        const firstNameElm = await waitForElement("input[name='first_name']", 1000);
+        if(firstNameElm) firstNameElm.value = params.get("firstName") || '';
+        
+        const emailElm = await waitForElement("input[name='email']", 1000);
+        if(emailElm) emailElm.value = params.get("email") || '';
+
+        const roleElm = await waitForElement("select[name='job_role']", 1000);
+        if(roleElm) roleElm.value = params.get("role") || '';
+
+        const companyElm = await waitForElement("input[name='company']", 1000);
+        if(companyElm) companyElm.value = params.get("company") || '';
+
+        const countryElm = await waitForElement("select[name='country']", 1000);
+        if(countryElm) countryElm.value = params.get("country") || '';
+
+        const postalCodeElm = await waitForElement("input[name='postal_code']", 1000);
+        if(postalCodeElm) postalCodeElm.value = params.get("postalCode") || '';
+
+        const userNameElm = await waitForElement("input[name='username']", 1000);
+        if(userNameElm) userNameElm.value = params.get("userName") || '';
+    } catch(e){
+        console.log(e);
+    }
+}
+main();
